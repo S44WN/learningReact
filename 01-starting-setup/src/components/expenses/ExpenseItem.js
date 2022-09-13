10162999627f36d55d783f03b249cdabf2464673
Sawan <itissaavn@gmail.com>
2022-09-13 11:46:12 +0530

    added a button

--- 01-starting-setup/src/components/expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/expenses/ExpenseItem.js
@@ -3,6 +3,10 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../ui/card";
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    return console.log("clickedd!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +14,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
