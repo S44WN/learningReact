8e743607b9eeb56158c670395b8d9e6dd45c6c80
Sawan <itissaavn@gmail.com>
2022-09-22 13:52:35 +0530

    added a folder learning react from w3s and html file for react in browser

--- 01-starting-setup/src/components/newExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/newExpense/ExpenseForm.js
@@ -57,6 +57,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expanseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -64,7 +67,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
 
         <div className="new-expense__control">
@@ -73,6 +80,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -83,6 +91,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
