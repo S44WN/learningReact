510786c216fb99cba506dc4b7af28e5ee717a58b
Sawan <itissaavn@gmail.com>
2022-12-29 21:55:14 +0530

    add new practice project & add AddUser conmponent

--- project-prac1/src/components/Users/AddUser.js
+++ project-prac1/src/components/Users/AddUser.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const AddUser = (props) => {
-  const addUserHandler = (event) => {
+  const addFormHandler = (event) => {
     event.preventDefault();
   };
+
   return (
-    <form onSubmit={addUserHandler}>
+    <form onSubmit={addFormHandler}>
       <label htmlFor="username">Username</label>
       <input id="username" type="text" />
       <label htmlFor="age">Age</label>
